8ce023956c94e227a323a5211990ff57c16b4f3c
Yonatan Dan <yonadan14@gmail.com>
2024-01-22 18:01:50 +0200

    implemented request to server on form submition

--- ResponseBuilder.java
+++ ResponseBuilder.java
@@ -37,7 +37,7 @@ public class ResponseBuilder {
 
     // default is head value is false . only when we get "HEAD" request type we change it to true
     public String buildResponse(int statusCode, String contentType, String content) {
-        return buildResponse(statusCode, contentType, content ,  false, false);
+        return buildResponse(statusCode, contentType, content , false, false);
     }
 
     public String buildResponse(int statusCode, String contentType, String content, Boolean isHead, Boolean isTrace) {
